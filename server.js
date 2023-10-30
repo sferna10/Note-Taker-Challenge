3951877c080b81d61d1600e01d709fbc286f8cbb
sferna10 <sferna10@email.cpcc.edu>
2023-10-29 23:34:28 -0400

    Debugging files

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const apiRoutes = require('./public/routes/apiRoutes');
-const htmlRoutes = require('./public/routes/htmlRoutes');
+const express = require("express");
+const apiRoutes = require("./public/routes/apiRoutes");
+const htmlRoutes = require("./public/routes/htmlRoutes");
 
 const app = express();
-const PORT = precess.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
