0cf8643c330b8c1ee145785e7cac8f1135332382
sferna10 <sferna10@email.cpcc.edu>
2023-11-26 11:44:07 -0500

    review index.js starter code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,49 +1,70 @@
-let noteTitle;
-let noteText;
-let saveNoteBtn;
-let newNoteBtn;
-let noteList;
+'Content-Type': 'application/json'
+},
+body: JSON.stringify(note),
+body: JSON.stringify(note)
+});
 
-if (window.location.pathname === '/notes') {
-    noteTitle = document.querySelector('.note-title');
-    noteText = document.querySelector('.note-textarea');
-    saveNoteBtn = document.querySelector('.save-note');
-    newNoteBtn = document.querySelector('.new-note');
-    noteList = document.querySelector('.list-container .list-group');
+const deleteNote = (id) =>
+fetch(`/api/notes/${id}`, {
+method: 'DELETE',
+headers: {
+  'Content-Type': 'application/json',
+},
+  'Content-Type': 'application/json'
 }
-//Show an element
-const show = (elem) => {
-    elem.style.display = 'inline';
+});
+
+const renderActiveNote = () => {
+hide(saveNoteBtn);
+hide(clearBtn);
+
+if (activeNote.id) {
+show(newNoteBtn);
+noteTitle.setAttribute('readonly', true);
+noteText.setAttribute('readonly', true);
+noteTitle.value = activeNote.title;
+noteText.value = activeNote.text;
+} else {
+hide(newNoteBtn);
+noteTitle.removeAttribute('readonly');
+noteText.removeAttribute('readonly');
+noteTitle.value = '';
+@@ -69,7 +75,7 @@ const renderActiveNote = () => {
+const handleNoteSave = () => {
+const newNote = {
+title: noteTitle.value,
+text: noteText.value,
+text: noteText.value
 };
-//Hide an element
-const hide = (elem) => {
-    elem.style.display = 'none';
+saveNote(newNote).then(() => {
+getAndRenderNotes();
+@@ -105,11 +111,16 @@ const handleNoteView = (e) => {
+// Sets the activeNote to and empty object and allows the user to enter a new note
+const handleNewNoteView = (e) => {
+activeNote = {};
+show(clearBtn);
+renderActiveNote();
 };
-//ActiveNote is used to keep track of the note in the textarea
-let activeNote = {};
 
-const getNotes = () =>
-    fetch('/api/notes', {
-    Method: 'GET',
-    headers: {
-        'Content-Type': 'applications/json',
-    },
-    });
+const handleRenderSaveBtn = () => {
+if (!noteTitle.value.trim() || !noteText.value.trim()) {
+// Renders the appropriate buttons based on the state of the form
+const handleRenderBtns = () => {
+show(clearBtn);
+if (!noteTitle.value.trim() && !noteText.value.trim()) {
+hide(clearBtn);
+} else if (!noteTitle.value.trim() || !noteText.value.trim()) {
+hide(saveNoteBtn);
+} else {
+show(saveNoteBtn);
+@@ -176,8 +187,8 @@ const getAndRenderNotes = () => getNotes().then(renderNoteList);
+if (window.location.pathname === '/notes') {
+saveNoteBtn.addEventListener('click', handleNoteSave);
+newNoteBtn.addEventListener('click', handleNewNoteView);
+noteTitle.addEventListener('keyup', handleRenderSaveBtn);
+noteText.addEventListener('keyup', handleRenderSaveBtn);
+clearBtn.addEventListener('click', renderActiveNote);
+noteForm.addEventListener('input', handleRenderBtns);
+}
 
-    const saveNotes = (note) =>
-    fetch('/api/notes', {
-    Method: 'POST',
-    headers: {
-        'Content-Type': 'applications/json',
-    },
-    body: JSON.stringify(note),
-    });
-
-    const deleteNotes = (id) =>
-    fetch(`/api/notes/${id}`, {
-    Method: 'DELETE',
-    headers: {
-        'Content-Type': 'applications/json',
-    },
-    });
-
+getAndRenderNotes();
