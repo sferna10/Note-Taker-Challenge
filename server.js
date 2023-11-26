d0e16de23219000ea52e8707b67375488f4a5143
sferna10 <sferna10@email.cpcc.edu>
2023-11-26 10:43:50 -0500

    Moving routes out of public file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const apiRoutes = require("./public/routes/apiRoutes");
-const htmlRoutes = require("./public/routes/htmlRoutes");
+const apiRoutes = require("./routes/apiroutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
